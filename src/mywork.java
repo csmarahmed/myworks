ea1fb184436f55c7c2aab893981584a13d51f92e
csmarahmed <csmarahmed@gmail.com>
2025-11-14 21:34:20 +0300

    Initi al commit

---
+++ src/mywork.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class mywork {
+    scanner scanner=new scanner(System.in);
+    public void Travelsal(){
+        int num1 []={1,2,3,4,5};
+        int num2 []=new int[5];
+        for (int i=0;i<num1.length;i++)
+            System.out.println(Arrays.toString(num2));
+    }
+    public void input(){
+
+
+    }
+}
