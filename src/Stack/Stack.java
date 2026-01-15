4bc8a6bc9d982e041695b5568e4700866378f5e7
csmarahmed <github@gmail.com>
2026-01-15 19:22:59 +0300

    2

---
+++ src/Stack/Stack.java
@@ -0,0 +1,11 @@
+ppackage Stack;
+
+public interface Stack<E> {
+    public boolean isEmpty();
+    public int size();
+    public void push(E data);
+    public E top();
+    public E  pop();
+
+
+}
